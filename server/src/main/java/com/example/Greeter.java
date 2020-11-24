19600bac1d40afe03bdc4407a804ea96c666bc25
Hulya GUNDUZ <hulyacgunduz@gmail.com>
2020-11-24 16:48:20 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,14 +1,24 @@
 package com.example;
 
-
+/**
+ * Classe contenant une fonction d'accueil.
+ */
 public class Greeter {
-
   
+  /**
+   * Constructeur vide.
+   */
   public Greeter() {
 
   }
 
+  /**
+   * Fonction qui salue une personne dont le nom est passé en paramètre.
+   * 
+   * @param someone (String) - Le nom de la personne saluée.
+   * @return Une phrase de salutation (String)
+   */
   public final String greet(final String someone) {
-    return String.format("Hello babacar, %s!", someone);
+    return String.format("Hello, Hulya %s!", someone);
   }
 }
